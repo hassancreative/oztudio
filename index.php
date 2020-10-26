282514b2d0d5edaf5d22cc88ae79fde828462fa4
Hassan <hassancreative9@gmail.com>
2020-10-27 01:46:49 +0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Oztudio || Home</title>
+    <!-- Roboto Fonts  -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+    <!-- Bootstrap 4.3.1 -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">    
+    <!-- Font Awesome -->
+    <script src="https://use.fontawesome.com/e88febfc35.js"></script>
+    <!-- Style Sheet -->
+    <link rel="stylesheet" href="assets/css/style.css" />
+</head>
+<body>
+
+<!-- Top Bar -->
+    <section class="top-bar">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="top-head-links">
+                        <ul class="default-list">
+                            <li><a href="javascript:void(0)"></a></li>
+                            <li><a href="javascript:void(0)"></a></li>
+                            <li><a href="javascript:void(0)"></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="top-head-links">
+                        <ul class="default-list">
+                            <li><a href="javascript:void(0)"></a></li>
+                            <li><a href="javascript:void(0)"></a></li>
+                            <li><a href="javascript:void(0)"></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+<!-- Top Bar -->
+
+<!-- Header -->
+
+<section>
+    <header>
+        <div class="container-fluid">
+            <div class="row">
+                
+            </div>
+        </div>
+    </header>
+</section>
+<!-- End Header -->
+
+
+
+ 
+
+
+<!-- Jquery Slim 3.3.1 -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<!-- Popper 1.14.7 -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<!-- Bootstrap JS 4.3.1 -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
